12e7cb97b4a5efea2fe747cfe0239bd4faecb106
Alejandra Duran <ad2002@princeton.edu>
2023-11-14 18:31:02 -0500

    back

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -261,7 +261,7 @@ static int FT_insertions(const char *pcPath, boolean isFile, void* FileContent, 
  
          /* fails if there is already a node (any type) with that 
         path at that depth */
-        printf("in else");
+        fprintf(stderr, "in else");
         if(ulIndex == ulDepth+1 && !Path_comparePath(oPPath,
                                          Node_getPath(oNCurr))) {
            Path_free(oPPath);
