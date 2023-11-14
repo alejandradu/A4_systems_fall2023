0120e7b60fbbda957514624d0f0166799170eb05
Siling Song <ss4799@princeton.edu>
2023-11-14 13:57:38 -0500

    trying to fix Node_NEW

--- 3FT/ft_client.c
+++ 3FT/ft_client.c
@@ -78,6 +78,7 @@ int main(void) {
   assert(FT_containsFile("1root/2child") == FALSE);
   assert(FT_containsDir("1root/2child/3gkid") == TRUE);
   assert(FT_containsFile("1root/2child/3gkid") == FALSE);
+  fprintf(stderr,"insertFile returns %d \n", FT_insertFile("1root/2second/3gfile", NULL, 0));
   assert(FT_insertFile("1root/2second/3gfile", NULL, 0) == SUCCESS);
   assert(FT_containsDir("1root/2second") == TRUE);
   assert(FT_containsFile("1root/2second") == FALSE);
